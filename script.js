a6e3680915c9e4eb9706f3014c7d96525cfbc8a8
Samiksha Anasane <samikshanasane@gmail.com>
2023-07-15 00:32:08 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>
+{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == "=")
+        {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C')
+        {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == '<=')
+        {
+            var value = document.getElementById("d").value;
+            document.getElementById("d").value = value.substr(0, value.length - 1);
+        }
+        else{
+            console.log(e.target) 
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
+    })
+})
